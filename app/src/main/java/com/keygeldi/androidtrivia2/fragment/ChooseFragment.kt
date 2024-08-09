465ca22bdc87b361f7004fc4b318e417b75a17ec
alper enes <alpereneskey@outlook.com>
2024-08-09 10:06:43 +0300

    9 Aralık Commit

---
+++ app/src/main/java/com/keygeldi/androidtrivia2/fragment/ChooseFragment.kt
@@ -0,0 +1,62 @@
+package com.keygeldi.androidtrivia2.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.keygeldi.androidtrivia2.R
+import com.keygeldi.androidtrivia2.databinding.FragmentChooseBinding
+
+
+class ChooseFragment : Fragment() {
+
+private lateinit var binding: FragmentChooseBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentChooseBinding.inflate(inflater, container, false)
+
+        binding.linearLayout3.setOnClickListener{
+            val bundle = Bundle()
+            bundle.putString("question_set","city_questions")
+            findNavController().navigate(R.id.action_chooseFragment2_to_gameFragment) //City
+        }
+        binding.linearLayout1.setOnClickListener{
+            val bundle = Bundle()
+            bundle.putString("question_set","flag_questions")
+            findNavController().navigate(R.id.action_chooseFragment2_to_gameFragment) //Flag
+        }
+
+        binding.linearLayout2.setOnClickListener{
+            val bundle = Bundle()
+            bundle.putString("question_set","3_questions")
+            findNavController().navigate(R.id.action_chooseFragment2_to_gameFragment)
+        }
+
+        binding.linearLayout4.setOnClickListener{
+            val bundle = Bundle()
+            bundle.putString("question_set","4_questions")
+            findNavController().navigate(R.id.action_chooseFragment2_to_gameFragment)
+        }
+
+        binding.linearLayout5.setOnClickListener{
+            val bundle = Bundle()
+            bundle.putString("question_set","5_questions")
+            findNavController().navigate(R.id.action_chooseFragment2_to_gameFragment)
+        }
+
+        binding.linearLayout6.setOnClickListener{
+            val bundle = Bundle()
+            bundle.putString("question_set","6_questions")
+            findNavController().navigate(R.id.action_chooseFragment2_to_gameFragment)
+        }
+
+        return binding.root
+    }
+
+}
